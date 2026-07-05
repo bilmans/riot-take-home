40dc4d8c4f24f35a9ef9e1ca5301b2795fa5279c
Bilal Manssouri <bilal.manssouri@pearltrees.net>
2026-07-05 11:42:03 +0200

    test: encrypt endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -23,6 +23,30 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 
+  describe('/encrypt (POST)', () => {
+    it('encrypts all depth-1 properties (base64), per TODO scenario', () => {
+      const input = {
+        name: 'John Doe',
+        age: 30,
+        contact: { email: 'john@example.com', phone: '123-456-7890' },
+      };
+      const expectedOutput = {
+        name: 'IkpvaG4gRG9lIg==',
+        age: 'MzA=',
+        contact:
+          'eyJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20iLCJwaG9uZSI6IjEyMy00NTYtNzg5MCJ9',
+      };
+
+      return request(app.getHttpServer())
+        .post('/encrypt')
+        .send(input)
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toEqual(expectedOutput);
+        });
+    });
+  });
+
   afterEach(async () => {
     await app.close();
   });
