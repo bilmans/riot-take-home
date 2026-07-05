a199eee0fa58aedcebb3c91c9f3fbe88af446e56
Bilal Manssouri <bilal.manssouri@pearltrees.net>
2026-07-05 12:14:22 +0200

    test: decrypt endpoint

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -47,6 +47,40 @@ describe('AppController (e2e)', () => {
     });
   });
 
+  describe('/decrypt (POST)', () => {
+    it('decrypts base64 values back to the original payload', () => {
+      return request(app.getHttpServer())
+        .post('/decrypt')
+        .send({
+          name: 'IkpvaG4gRG9lIg==',
+          age: 'MzA=',
+          contact:
+            'eyJlbWFpbCI6ImpvaG5AZXhhbXBsZS5jb20iLCJwaG9uZSI6IjEyMy00NTYtNzg5MCJ9',
+        })
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            name: 'John Doe',
+            age: 30,
+            contact: { email: 'john@example.com', phone: '123-456-7890' },
+          });
+        });
+    });
+
+    it('leaves non-encrypted values unchanged', () => {
+      return request(app.getHttpServer())
+        .post('/decrypt')
+        .send({ name: 'IkpvaG4gRG9lIg==', birth_date: '1998-11-19' })
+        .expect(201)
+        .expect((res) => {
+          expect(res.body).toEqual({
+            name: 'John Doe',
+            birth_date: '1998-11-19',
+          });
+        });
+    });
+  });
+
   afterEach(async () => {
     await app.close();
   });
